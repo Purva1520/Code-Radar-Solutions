956602488b3551b1a7c9a68f0b27d8ed89bff5d3
Purva1520 <74709497+Purva1520@users.noreply.github.com>
2025-02-05 09:26:02 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    char l;
+    scanf("%c",&l);
+    if(l=='R')
+    {
+        printf("Stop");
+    }
+    else if(l=='G')
+    {
+        printf("Go");
+    }
+    else if(l=='Y')
+    {
+        printf("Slow Down");
+    }
+    else
+    {
+        printf("Invalid input");
+    }
+return 0;
+}
